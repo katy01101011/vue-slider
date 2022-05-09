a739d6155c967a54602f0145ba320322b35ade26
katy01101011 <katy.91560@gmail.com>
2022-05-09 08:35:35 +0200

    non funzionana :(

--- js/app.js
+++ js/app.js
@@ -1,39 +1,42 @@
-
-
+Vue.config.devtools = true;
 const app = new Vue(
     {
         el: '#root',
         data: {
             countries: [
                 {
-                    image: 'img/01.jpg',
+                    image: './img/01.jpg',
                     title: 'Svezia',
                     text: 'Svezia: un Paese che, forse, consideriamo poco dal punto di vista turistico. Eppure, certe città della Svezia sono ottime mete per una vacanza un po’ diversa dal solito, alla scoperta di una cultura speciale!'
                 },
                 {
-                    image: 'img/02.jpg',
+                    image: './img/02.jpg',
                     title: 'Svizzera',
                     text: 'Un viaggio in Svizzera è sinonimo di relax, natura, sport all’aria aperta… ma anche arte e cultura, perché le città svizzere hanno tantissimo da offrire!'
                 },
                 {
-                    image: 'img/03.jpg',
+                    image: './img/03.jpg',
                     title: 'Gran Bretagna',
                     text: 'Anche le meraviglie naturali della Gran Bretagna raccontano del glorioso passato del Paese, tra gole impervie, spettacolari anfiteatri calcarei e maestose scogliere gessose.'
                 },
                 {
-                    image: 'img/04.jpg',
+                    image: './img/04.jpg',
                     title: 'Germania',
                     text: 'A volte non serve andare troppo lontano per vivere un magnifico viaggio on the road. La Strada Romantica in Germania, ad esempio, è ricca di tappe meravigliose… per le coppie e non solo!'
                 },
                 {
-                    image: 'img/05.jpg',
+                    image: './img/05.jpg',
                     title: 'Paradise',
                     text: 'Ché per tal donna, giovinetto, in guerra del padrecorse, a cui, come a la morte, la porta del piacer nessun diserra; e dinanzi a la sua spiritual corte et coram patre le si fece unito; poscia di dì in dì l\'amò più forte'
                 }
             ],
+
+            pic: this[0].image,
+            picTitle: this.title,
+            picText: this.text
         },
         methods: {
-
+            
         }
     }
 )
